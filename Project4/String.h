2780326f1dffcde6d601bb5015785525da5c0aa3
Karim Baker <bin.karim.bin@gmail.com>
2025-01-19 02:33:01 +0200

    Задание 1. String

---
+++ Project4/String.h
@@ -0,0 +1,51 @@
+#pragma once
+
+#include <iostream>
+
+class String {
+private:
+    char* str;
+    size_t length;
+
+public:
+    String() : String(80) {}
+
+    String(size_t size) : length(size) {
+        str = new char[length + 1];
+        for (size_t i = 0; i <= length; ++i) {
+            str[i] = '\0';
+        }
+    }
+
+    String(const char* input) {
+        length = 0;
+        while (input[length] != '\0') {
+            ++length;
+        }
+        str = new char[length + 1];
+        for (size_t i = 0; i < length; ++i) {
+            str[i] = input[i];
+        }
+        str[length] = '\0';
+    }
+
+    String(const String& other) : length(other.length) {
+        str = new char[length + 1];
+        for (size_t i = 0; i <= length; ++i) {
+            str[i] = other.str[i];
+        }
+    }
+
+    ~String() {
+        delete[] str;
+    }
+
+    void input() {
+        std::cout << "Enter string: ";
+        std::cin.getline(str, length + 1);
+    }
+
+    void output() const {
+        std::cout << "String: " << str << std::endl;
+    }
+};
